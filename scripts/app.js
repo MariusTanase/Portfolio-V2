159b768fb6575ffecc57c194c17905c82c940b43
MariusTanase <pulsemusic8@hotmail.com>
2022-06-10 18:26:56 +0100

    Dynamically changing HR Width based on position

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,16 @@
 // selecting navbar
 const header = document.querySelector('header');
-
+const hr = document.querySelector('hr');
 //if header position is bigger than 0 then add background color to header
 window.onscroll = function () {
     if (window.pageYOffset > 0) {
         header.classList.add('bg-dark');
+        hr.style.width = '100%';
+
     } else {
         header.classList.remove('bg-dark');
+        hr.style.width = '80%';
+
     }
 }
+
